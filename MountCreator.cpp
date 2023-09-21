8f7a25e6c1835cba4a6dea43fb97dd790850624e
nikpan1 <nikodem.panknin@gmail.com>
2023-09-21 23:58:45 +0200

    new debug form

--- MountCreator.cpp
+++ MountCreator.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -10,13 +11,14 @@ std::string bitMask = "   100001";
 #define _test std::cout << "test\n";
 
 std::string convert(const std::string &origin) {
+  std::cout << origin << "|\n";
   std::string mount = origin;
   std::string number = origin.substr(1, 2);
   std::cout << number << "-D\n";
   // int n = std::stoi(number) - 2;
   int n = 2;
   number = "M" + std::to_string(n);
-
+  std::cout << "Num = " << number << "\nmount= " << mount << "\n";
   mount.insert(0, number);
   mount.insert(mount.size() - bitMask.size(), bitMask);
 
@@ -31,6 +33,7 @@ void validate(bool result,
   std::cerr << errorMessage;
   std::exit(-1);
 }
+#define writeLine(output, line) output << line << '\n';
 
 int main(int argc, char *argv[]) {
   validate(argc != 2, "Wrong amount of arguments.\n");
@@ -46,36 +49,28 @@ int main(int argc, char *argv[]) {
   std::string line;
 
   // find first line of origin
-  while (std::getline(readedFile, line) ||
-         !(ORIGIN_START.compare(line.substr(0, 1)))) {
-    _test tempFile << line;
-  }
+  while (std::getline(readedFile, line) &&
+         !(ORIGIN_START.compare(line.substr(0, 1))))
+    writeLine(tempFile, line);
 
   mountLines.push_back(convert(line));
-  while (std::getline(readedFile, line) && line[0] == 'O') {
-    _test mountLines.push_back(convert(line));
-  }
+  while (std::getline(readedFile, line) && line[0] == 'O')
+    mountLines.push_back(convert(line));
 
   // go to first line of mount
-  while (std::getline(readedFile, line) && !(line[0] == 'i')) {
-    tempFile << line;
-  }
-  for (auto _line : mountLines) {
-    tempFile << line;
-  }
+  while (std::getline(readedFile, line) && !(line[0] == 'i'))
+    writeLine(tempFile, line);
+
+  for (auto _mountLine : mountLines)
+    tempFile << _mountLine;
 
   tempFile.close();
   readedFile.close();
 
   // Remove the original file and replace it with the temporary file
-  if (std::remove(readedFilename) != 0) {
-    std::cerr << "Error deleting the original file.\n";
-    return -1;
-  }
-  if (std::rename(tempFilename, readedFilename) != 0) {
-    std::cerr << "Error replacing the original file with the temporary file.\n";
-    return -1;
-  }
-
+  validate(std::remove(readedFilename) != 0,
+           "Deleting the original file not sucessfull.\n");
+  validate(std::rename(tempFilename, readedFilename) != 0,
+           "Replacing the files not sucessfull.\n");
   return 1;
 }
