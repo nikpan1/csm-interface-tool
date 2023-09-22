f2241bdec28a7ab73769e66d4bd8ff30cfe3259a
nikpan1 <nikodem.panknin@gmail.com>
2023-09-22 13:10:24 +0200

    feeder tpes written down - STEUER

--- MountCreator.cpp
+++ MountCreator.cpp
@@ -1,11 +1,32 @@
+#include <algorithm>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
+enum FEEDERS {
+  F11 = 11, // LED-green, SMD1206
+  F12 = 12, // LED-red, SMD1206
+  F13 = 13, // 100nF/50V
+  F14 = 14, // 220nF/250V
+  F15 = 15, // 1nF/600V
+  F16 = 16, // 2.2kOhm/1%
+  F17 = 17, // 1kOhm/1%
+  F18 = 18, // 2.2nF/500V
+  F19 = 19, // 47nF/50V
+  F20 = 20, // 10nF/630V
+  F21 = 21, // 10nF/50V
+  F22 = 22, // 100uF/25V
+  F23 = 23, // 390pF/6300V
+  F24 = 24, // 47nF/500V
+  F25 = 25  // 22pF/50V
+};
+
 std::string ORIGIN_START = "O2";
 std::string BADMARK_START = "i0";
-std::string bitMask = "   100001\n";
+std::string bitMask = "   100001";
+
 #define tempFilename "temp.txt"
 #define _test std::cout << "test\n";
 #define writeLine(output, line)                                                \
@@ -14,6 +35,29 @@ std::string bitMask = "   100001\n";
     std::cout << line << "\n";                                                 \
     output << line << '\n';                                                    \
   }
+
+std::string generateBitmask(uint8_t feeder, uint8_t header, bool skip) {
+  std::string result = bitMask;
+  // header 1/2
+
+  std::string buffer;
+
+  // setting feeder type
+  buffer = std::to_string(feeder);
+  result.replace(result.size() - 6, buffer.size(), buffer);
+
+  // setting header type
+  buffer = std::to_string(header);
+  result.replace(result.size() - 1, buffer.size(), buffer);
+
+  // setting skip type
+  buffer = std::to_string(skip);
+  result.replace(result.size() - 4, buffer.size(), buffer);
+
+  result = result + "\n";
+
+  return result;
+}
 
 std::string convert(const std::string &origin) {
   std::string mount = origin;
@@ -26,7 +70,8 @@ std::string convert(const std::string &origin) {
   mount[2] = ' ';
   mount.replace(0, (n < 10) ? 2 : 3, mountNumber);
 
-  mount.replace(mount.size() - bitMask.size(), bitMask.size(), bitMask);
+  std::string _bitmask = generateBitmask(3, 1, false);
+  mount.replace(mount.size() - _bitmask.size(), _bitmask.size(), _bitmask);
 
   return mount;
 }
